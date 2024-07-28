5a2255b8a2bad9980f964001f63423bc1a3ffc61
Ashlesh31 <122680092+Ashlesh31@users.noreply.github.com>
2024-07-29 04:44:17 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,35 @@
+from django.db import models
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    def __str__(self):
+        return self.name   
+    
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateTimeField(auto_now_add=True)
+
+
+class Meta:
+    unique_together = ('student', 'course')
+    def __str__(self):
+        return f"{self.student} enrolled in {self.course}"
+
+
+class Project(models.Model): 
+        student = models.OneToOneField(Student, on_delete=models.CASCADE) 
+        topic = models.CharField(max_length=200) 
+        languages_used = models.CharField(max_length=200) 
+        duration = models.PositiveIntegerField(help_text="Duration in weeks") 
+        def __str__(self): 
+            return f"{self.topic} by {self.student}"
